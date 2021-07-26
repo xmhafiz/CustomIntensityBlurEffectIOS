b29fd9b745d37b678e92b3d2b634bec3b5c2e0fd
Mohd Hafiz <xmhafiz@gmail.com>
2021-07-26 13:35:39 +0800

    init project

---
+++ CustomBlurBackground/InfoViewController.swift
@@ -0,0 +1,18 @@
+//  InfoViewController.swift
+import UIKit
+
+class InfoViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+    
+    func setupView() {
+        //
+    }
+    
+    @IBAction func dismissAction(_ sender: Any) {
+        //
+    }
+}
---
+++ CustomBlurBackground/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  CustomBlurBackground
+//
+//  Created by Hafiz on 26/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func showDetailAction(_ sender: Any) {
+        //
+    }
+}
+
